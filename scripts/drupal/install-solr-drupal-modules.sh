12e341093dafc80d85f65e5d5dfe6c1e34ecd702
Gavin Morris <gavin@born-digital.com>
2020-02-20 13:54:39 -0500

    Updated installer script with Drupal site install. Added php.env for install. Moved around envs accordingly

--- scripts/drupal/install-solr-drupal-modules.sh
+++ scripts/drupal/install-solr-drupal-modules.sh
@@ -4,39 +4,45 @@
 # composer.json and composer.lock file were from:
 # git clone https://github.com/drupal/recommended-project.git
 
+# TO DO: Replace this block below with a git clone in MVP2
+# TO DO: Determine what elements of the following composer.json files below go into the demo.
+# https://github.com/drupal/recommended-project.git
+# https://github.com/drupal-composer/drupal-project/blob/8.x/composer.json
+# https://github.com/Islandora/drupal-project/blob/8.x-1.x/composer.json
+# scripts/drupal.composer.json (checked into ISLE git repo resulting from MVP 1)
+
 echo "Copying composer files to /var/www/html"
 cp /scripts/drupal/composer.json /var/www/html/
 cp /scripts/drupal/composer.lock /var/www/html/
 chmod 755 /var/www/html/composer.*
 
+# TO DO: mkdir theme dir and other necessary sub-folders
+
 cd /var/www/html || exit
 
-echo "composer install"
-composer install
-
 echo "composer update && installing Drupal console"
-chmod u+w web/sites/default
 composer update
+chmod u+w web/sites/default
 composer require drupal/console:~1.0 --prefer-dist --optimize-autoloader
 
 echo "Installing Drupal site"
-drupal site:install $DRUPAL_INSTALL_TYPE  \
---langcode=$DRUPAL_LANGUAGE  \
---db-type=$DB_DRIVER  \
---db-host=$DB_HOST  \
---db-name=$DB_NAME  \
---db-user=$DB_USER  \
---db-pass=$DB_PASSWORD  \
---db-port=$DB_PORT  \
---site-name=$DRUPAL_SITE_NAME  \
---site-mail=$DRUPAL_SITE_MAIL  \
---account-name=$DRUPAL_USER  \
---account-mail=$DRUPAL_USER_EMAIL  \
---account-pass=$DRUPAL_USER_PASSWORD
+drupal site:install "$DRUPAL_INSTALL_TYPE"  \
+--langcode="$DRUPAL_LANGUAGE"  \
+--db-type="$DB_DRIVER"  \
+--db-host="$DB_HOST"  \
+--db-name="$DB_NAME"  \
+--db-user="$DB_USER" \
+--db-pass="$DB_PASSWORD"  \
+--db-port="$DB_PORT"  \
+--db-prefix="$DB_PREFIX" \
+--site-name="$DRUPAL_SITE_NAME"  \
+--site-mail="$DRUPAL_SITE_MAIL"  \
+--account-name="$DRUPAL_USER"  \
+--account-mail="$DRUPAL_USER_EMAIL"  \
+--account-pass="$DRUPAL_USER_PASSWORD"
 
-echo "Updating Composer and dependencies"
+echo "Fixing perms on web/sites/default"
 chmod u+w web/sites/default
-composer update
 
 echo "Disabling & removing Drupal module - search"
 drupal module:uninstall -y search
