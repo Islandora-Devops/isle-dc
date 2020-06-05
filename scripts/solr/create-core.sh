2adebb9d0e1ababe76082474a33fdbcaa7ac3e6d
Gavin Morris <gavin@born-digital.com>
2020-06-05 11:59:51 -0400

    Updating SOLR core back to ISLANDORA to match buildkit. Cleaning up README fragments.

--- scripts/solr/create-core.sh
+++ scripts/solr/create-core.sh
@@ -5,14 +5,14 @@ cd /var/www/drupal/ || exit
 
 drush -y solr-gsc default_solr_server /tmp/solr_config.zip 7.1
 
-mkdir -p /opt/solr/server/solr/islandora/conf || true
+mkdir -p /opt/solr/server/solr/ISLANDORA/conf || true
 
-mkdir -p /opt/solr/server/solr/islandora/data || true
+mkdir -p /opt/solr/server/solr/ISLANDORA/data || true
 
-unzip -o /tmp/solr_config.zip -d /opt/solr/server/solr/islandora/conf
+unzip -o /tmp/solr_config.zip -d /opt/solr/server/solr/ISLANDORA/conf
 
 # The uid:gid "100:1000" is "solr:solr" inside of the solr container.
 
-chown -R 100:1000 /opt/solr/server/solr/islandora
+chown -R 100:1000 /opt/solr/server/solr/ISLANDORA
 
-curl -s "http://solr:8983/solr/admin/cores?action=CREATE&name=islandora&instanceDir=islandora&config=solrconfig.xml&dataDir=data" &> /dev/null
+curl -s "http://solr:8983/solr/admin/cores?action=CREATE&name=ISLANDORA&instanceDir=ISLANDORA&config=solrconfig.xml&dataDir=data" &> /dev/null
