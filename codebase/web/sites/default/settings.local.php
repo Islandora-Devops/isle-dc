361d2c0f6ce91c72095938c1a3569546a6c8743c
Aaron Birkland <apb@jhu.edu>
2020-11-18 00:04:20 -0500

    Add config for s3fs

--- codebase/web/sites/default/settings.local.php
+++ codebase/web/sites/default/settings.local.php
@@ -19,3 +19,14 @@ $databases['default']['default']['prefix'] = '';
 $databases['default']['default']['driver'] = getenv('DRUPAL_DEFAULT_DB_DRIVER') ?: 'mysql';
 $databases['default']['default']['namespace']  = 'Drupal\\Core\\Database\\Driver\\';
 $databases['default']['default']['namespace'] .= $databases['default']['default']['driver'];
+
+$settings['s3fs.access_key'] = getenv('DRUPAL_DEFAULT_S3_ACCESS_KEY');
+$settings['s3fs.secret_key'] = getenv('DRUPAL_DEFAULT_S3_SECRET_KEY');
+$config['s3fs.settings']['bucket'] = getenv('DRUPAL_DEFAULT_S3_BUCKET') ?: 'idc';
+$config['s3fs.settings']['hostname'] = getenv('DRUPAL_DEFAULT_S3_HOSTNAME');
+$config['s3fs.settings']['use_s3_for_private'] = TRUE;
+$config['s3fs.settings']['use_s3_for_public'] = FALSE;
+$config['s3fs.settings']['use_cname'] = getenv('DRUPAL_DEFAULT_S3_USE_CNAME') ?: false;
+$config['s3fs.settings']['use_customhost'] = getenv('DRUPAL_DEFAULT_S3_USE_CUSTOMHOST') ?: false;
+$config['s3fs.settings']['use_path_style_endpoint'] = getenv('DRUPAL_DEFAULT_S3_USE_PATH_STYLE_ENDPOINT') ?: false;
+$config['file_private_path'] = getenv('DRUPAL_DEFAULT_S3_PRIVATE_PATH') ?: 'pr';
