22a61f2d94f2f7d11b4559981dc65d7b929ff020
Aaron Birkland <apb@jhu.edu>
2021-03-11 16:59:52 -0500

    Use correct coalescing operator for default values

--- codebase/web/sites/default/settings.local.php
+++ codebase/web/sites/default/settings.local.php
@@ -5,12 +5,12 @@ $settings['hash_salt'] = getenv('DRUPAL_DEFAULT_SALT');
 
 $settings['config_sync_directory'] = '/var/www/drupal/config/sync';
 
-$databases['default']['default']['database'] =  getenv('DRUPAL_DEFAULT_DB_NAME') ?? 'drupal+default';
-$databases['default']['default']['username'] =  getenv('DRUPAL_DEFAULT_DB_USER') ?? 'drupal_default';
+$databases['default']['default']['database'] =  getenv('DRUPAL_DEFAULT_DB_NAME') ?: 'drupal_default';
+$databases['default']['default']['username'] =  getenv('DRUPAL_DEFAULT_DB_USER') ?: 'drupal_default';
 $databases['default']['default']['password'] =  getenv('DRUPAL_DEFAULT_DB_PASSWORD');
-$databases['default']['default']['host'] =  getenv('DRUPAL_DEFAULT_DB_HOST') ?? 'database';
-$databases['default']['default']['port'] =  getenv('DRUPAL_DEFAULT_DB_PORT') ?? '3306';
+$databases['default']['default']['host'] =  getenv('DRUPAL_DEFAULT_DB_HOST') ?: 'database';
+$databases['default']['default']['port'] =  getenv('DRUPAL_DEFAULT_DB_PORT') ?: '3306';
 $databases['default']['default']['prefix'] = '';
-$databases['default']['default']['driver'] = getenv('DRUPAL_DEFAULT_DB_DRIVER') ?? 'mysql';
+$databases['default']['default']['driver'] = getenv('DRUPAL_DEFAULT_DB_DRIVER') ?: 'mysql';
 $databases['default']['default']['namespace']  = 'Drupal\\Core\\Database\\Driver\\';
 $databases['default']['default']['namespace'] .= $databases['default']['default']['driver'];
