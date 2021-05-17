433f70fcbc3bf6d2df4394ede5eb2721489ceb51
Aaron Birkland <apb@jhu.edu>
2021-05-17 00:41:27 -0400

    code clean-up

--- tests/12-migration-derivative-tests/testcafe/formats.init.js
+++ tests/12-migration-derivative-tests/testcafe/formats.init.js
@@ -1,17 +1,28 @@
-import {adminUser} from './roles.js';
-import {doMigration} from './util.js';
-import {migrationType} from './util.js';
+import { adminUser } from "./roles.js";
+import { doMigration } from "./util.js";
+import { migrationType } from "./util.js";
 
 fixture`Ingest Media Formats`
-    .page`https://islandora-idc.traefik.me/migrate_source_ui`
-    .beforeEach(async t => {
-        await t
-            .useRole(adminUser);
-    });
+  .page`https://islandora-idc.traefik.me/migrate_source_ui`.beforeEach(
+  async (t) => {
+    await t.useRole(adminUser);
+  }
+);
 
-
-test('Migrate Images for testing format support', async t => {
-    await doMigration(t, migrationType.NEW_COLLECTION, './migrations/media-format-collection.csv');
-    await doMigration(t, migrationType.NEW_ITEM, './migrations/media-format-objects.csv');
-    await doMigration(t, migrationType.NEW_MEDIA_IMAGE, './migrations/media-format-file.csv');
+test("Migrate Images for testing format support", async (t) => {
+  await doMigration(
+    t,
+    migrationType.NEW_COLLECTION,
+    "./migrations/media-format-collection.csv"
+  );
+  await doMigration(
+    t,
+    migrationType.NEW_ITEM,
+    "./migrations/media-format-objects.csv"
+  );
+  await doMigration(
+    t,
+    migrationType.NEW_MEDIA_IMAGE,
+    "./migrations/media-format-file.csv"
+  );
 });
