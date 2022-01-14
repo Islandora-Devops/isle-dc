ee8029439c404c73c275140f2706d8ba7faafb9b
Don Richards <highlander411@gmail.com>
2022-01-14 09:51:10 -0500

    Adds check before running & add drupal console here

--- scripts/patch_views.sh
+++ scripts/patch_views.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-# Reinstall views
-/var/www/drupal/vendor/drupal/console/bin/drupal views:debug | grep Enabled | awk '{system("/var/www/drupal/vendor/drupal/console/bin/drupal views:disable " $1)}'
-/var/www/drupal/vendor/drupal/console/bin/drupal views:debug | grep Disabled | awk '{system("/var/www/drupal/vendor/drupal/console/bin/drupal views:enable " $1)}'
+# To be precise on the error message that matches the error this should address.
+ERROR_MESSAGE=$(drush watchdog:show --severity=Error | grep 'cron\|php' | grep 'A valid cache entry key is required' | awk '{print $6}')
 
-# Install devel.
-composer require 'drupal/devel:^4.1' -W && drush pm:enable -y devel
-drush dev:reinstall islandora
+# If error message equals to "No such file or directory", then exit.
+if [[ $ERROR_MESSAGE == *'InvalidArgumentException'* ]]; then
+	# Install Drupal Console.
+	composer require drupal/console:~1.0 --prefer-dist --optimize-autoloader -W
 
-# Clear caches
-/var/www/drupal/vendor/drupal/console/bin/drupal cache:rebuild
-/var/www/drupal/vendor/drupal/console/bin/drupal cr all
-/var/www/drupal/vendor/drupal/console/bin/drupal node:access:rebuild
-drush cron
+	# Reinstall views
+	/var/www/drupal/vendor/drupal/console/bin/drupal views:debug | grep Enabled | awk '{system("/var/www/drupal/vendor/drupal/console/bin/drupal views:disable " $1)}'
+	/var/www/drupal/vendor/drupal/console/bin/drupal views:debug | grep Disabled | awk '{system("/var/www/drupal/vendor/drupal/console/bin/drupal views:enable " $1)}'
+
+	# Install devel.
+	composer require 'drupal/devel:^4.1' -W && drush pm:enable -y devel
+	drush dev:reinstall islandora
+
+	# Clear caches
+	/var/www/drupal/vendor/drupal/console/bin/drupal cache:rebuild
+	/var/www/drupal/vendor/drupal/console/bin/drupal cr all
+	/var/www/drupal/vendor/drupal/console/bin/drupal node:access:rebuild
+	drush cron
+fi
