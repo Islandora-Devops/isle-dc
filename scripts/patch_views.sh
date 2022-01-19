07017d32479afb9d9456b33241632691424c2207
Don Richards <2738244+DonRichards@users.noreply.github.com>
2022-01-19 14:16:48 -0500

    Improve queries

--- scripts/patch_views.sh
+++ scripts/patch_views.sh
@@ -2,7 +2,7 @@
 set -e
 
 # To be precise on the error message that matches the error this should address.
-ERROR_MESSAGE=$(drush watchdog:show --severity=Error | grep 'cron\|php' | grep 'A valid cache entry key is required' | awk '{print $6}')
+ERROR_MESSAGE=$(drush watchdog:show --severity=Error --filter="InvalidArgumentException: A valid cache entry key is required" | awk '{print $6}')
 
 # If error message equals to "No such file or directory", then exit.
 if [[ $ERROR_MESSAGE == *'InvalidArgumentException'* ]]; then
@@ -10,8 +10,8 @@ if [[ $ERROR_MESSAGE == *'InvalidArgumentException'* ]]; then
 	composer require drupal/console:~1.0 --prefer-dist --optimize-autoloader -W
 
 	# Reinstall views
-	/var/www/drupal/vendor/drupal/console/bin/drupal views:debug | grep Enabled | awk '{system("/var/www/drupal/vendor/drupal/console/bin/drupal views:disable " $1)}'
-	/var/www/drupal/vendor/drupal/console/bin/drupal views:debug | grep Disabled | awk '{system("/var/www/drupal/vendor/drupal/console/bin/drupal views:enable " $1)}'
+	/var/www/drupal/vendor/drupal/console/bin/drupal debug:views --status='Enabled' | awk '{system("/var/www/drupal/vendor/drupal/console/bin/drupal views:disable " $1)}'
+	/var/www/drupal/vendor/drupal/console/bin/drupal debug:views --status='Disabled' | awk '{system("/var/www/drupal/vendor/drupal/console/bin/drupal views:enable " $1)}'
 
 	# Install devel.
 	composer require 'drupal/devel:^4.1' -W && drush pm:enable -y devel
